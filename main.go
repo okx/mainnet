db6a4e0014bcd8e0011452a387b195156e9ce736
zhengchao <chao.zheng@okcoin.com>
2021-01-11 15:09:56 +0800

    submit config

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ const (
 	genesisFile     = "okexchain-genesis.json"
 
 	denomination      = common.NativeToken
-	timeGenesisString = "2020-12-04 04:00:00 -0000 UTC"
+	timeGenesisString = "2021-01-10 04:00:00 -0000 UTC"
 )
 
 // constants but can't use `const`
