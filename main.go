9997e8066d02f43ea3802c261304a56e1a6cef92
zhengchao <chao.zheng@okcoin.com>
2021-01-14 19:28:06 +0800

    modify start time

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ const (
 	genesisFile     = "genesis.json"
 
 	denomination      = common.NativeToken
-	timeGenesisString = "2021-01-14 13:00:00 -0000 UTC"
+	timeGenesisString = "2021-01-15 12:00:00 -0000 UTC"
 )
 
 // constants but can't use `const`
